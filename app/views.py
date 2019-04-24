aa29b81580b9fca053651f5fbbf6e46f298161d1
Aleksey <lexa.merkylov99@gmail.com>
2019-04-24 15:16:34 +0300

    +

--- app/views.py
+++ app/views.py
@@ -15,16 +15,19 @@ from app import app
 @app.route('/login', methods = ['GET', 'POST'])
 @oid.loginhandler
 def login():
-  if g.user is not None and g.user.is_authenticated():
+
+    if g.user is not None and g.user.is_authenticated:
       return redirect(url_for('index'))
-  form = LoginForm()
-  if form.validate_on_submit():
-      session['remember_me'] = form.remember_me.data
-      return oid.try_login(form.openid.data, ask_for = ['nickname', 'email'])
-  return render_template('login.html',
-      title = 'Sign In',
-      form = form,
-      providers = app.config['OPENID_PROVIDERS'])
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        session['remember_me'] = form.remember_me.data
+        return oid.try_login(form.openid.data, ask_for = ['nickname', 'email'])
+        
+    return render_template('login.html',
+        title = 'Sign In',
+        form = form,
+        providers = app.config['OPENID_PROVIDERS'])
 
 #---------------------------------------------------------------------#
 @oid.after_login
@@ -50,7 +53,7 @@ def after_login(resp):
 @app.before_request
 def before_request():
     g.user = current_user
-    
+
 #---------------------------------------------------------------------#
 @app.route('/signup', methods = ['GET', 'POST'])
 def signin():
