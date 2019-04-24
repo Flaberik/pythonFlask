0a43015defaed734703dc55908e30661cdfe0fbd
unknown <lexa.merkylov99@gmail.com>
2019-04-24 11:25:19 +0300

    +

--- app/models.py
+++ app/models.py
@@ -16,6 +16,7 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime)
+    author  = db.Column(db.String(64), index = True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
